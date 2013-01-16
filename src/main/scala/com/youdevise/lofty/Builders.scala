b9256cdf2b5600429fcc7e085ec392d84a1fa94c
Dominic Fox <dominic.fox@gmail.com>
2013-01-16 12:40:54 +0000

    Break up spec into smaller tests

--- src/main/scala/com/youdevise/lofty/Builders.scala
+++ src/main/scala/com/youdevise/lofty/Builders.scala
@@ -6,20 +6,37 @@ import Scalaz._
 
 object Builders {
 
+  type Transcripts = Map[BuildTarget[_], Seq[RecordedCall]]
+
   trait Builder[T] {
     def buildFrom(properties: Map[String, _]): T
   }
 
-  private val emptyRecording = Recording(Seq.empty[RecordedCall])
+  private[this] val emptyRecording = Recording(Seq.empty[RecordedCall])
 
   def builder[T](implicit builder: Builder[T]) = state[Recording, BuildTarget[T]] { (recording: Recording) =>
     (recording, new BuildTarget[T](builder))
   }
 
-  def buildFrom[T](state: State[Recording, BuildTarget[T]]): T = {
+  private[this] def transcriptsFor[A](state: State[Recording, A]): (Transcripts, A) = {
     val (recording, target) = state(emptyRecording)
     val transcripts = recording.calls.groupBy(_.target)
+    (transcripts, target)
+  }
+
+  def buildFrom[T](state: State[Recording, Buildable[T]]): T = {
+    val (transcripts, target) = transcriptsFor(state)
     target.buildFrom(transcripts)
+  }
+
+  def buildFrom[T1, T2](state: State[Recording, (Buildable[T1], Buildable[T2])]): (T1, T2) = {
+    val (transcripts, (target1, target2)) = transcriptsFor(state)
+    (target1.buildFrom(transcripts), target2.buildFrom(transcripts))
+  }
+
+  def buildFrom[T1, T2, T3](state: State[Recording, (Buildable[T1], Buildable[T2], Buildable[T3])]): (T1, T2, T3) = {
+    val (transcripts, (target1, target2, target3)) = transcriptsFor(state)
+    (target1.buildFrom(transcripts), target2.buildFrom(transcripts), target3.buildFrom(transcripts))
   }
 }
 
@@ -31,7 +48,17 @@ sealed case class RecordedCall(target: BuildTarget[_], methodName: String, param
 
 sealed case class Recording(val calls: Seq[RecordedCall])
 
-sealed class BuildTarget[T](val builder: Builder[T]) extends Dynamic {
+trait Buildable[T] {
+  protected def reify(transcripts: Transcripts, value: Any): Any = value match {
+    case target: BuildTarget[_] => target.buildFrom(transcripts)
+    case targets: Iterable[_] => targets.map(reify(transcripts, _))
+    case _ => value
+  }
+
+  def buildFrom(transcripts: Transcripts): T
+}
+
+sealed class BuildTarget[T](val builder: Builder[T]) extends Buildable[T] with Dynamic {
   private def record(name: String, args: Any*) = {
     val target = this
     state[Recording, BuildTarget[T]] { (recording:Recording) => (
@@ -40,15 +67,9 @@ sealed class BuildTarget[T](val builder: Builder[T]) extends Dynamic {
     }
   }
 
-  private[this] def reify(transcripts: Map[BuildTarget[_], Seq[RecordedCall]], value: Any): Any = value match {
-    case target: BuildTarget[_] => target.buildFrom(transcripts)
-    case targets: Iterable[_] => targets.map(reify(transcripts, _))
-    case _ => value
-  }
-
   def applyDynamic(name: String)(args: Any*) = record(name, args:_*)
 
-  def buildFrom(transcripts: Map[BuildTarget[_], Seq[RecordedCall]]): T = {
+  override def buildFrom(transcripts: Transcripts): T = {
     val myTranscript = transcripts(this)
     val properties = myTranscript.map(_.property).toMap
     val reifiedProperties = properties.mapValues(reify(transcripts, _))
--- src/test/scala/com/youdevise/lofty/BuildersSpec.scala
+++ src/test/scala/com/youdevise/lofty/BuildersSpec.scala
@@ -28,41 +28,102 @@ class BuildersSpec extends Specification {
   }
 
   """A builder""" should {
-    """Build an object using all method calls captured within the recorder monad""" in {
+    """Build an object using the method calls captured within the recorder monad""" in {
 
-      val maudlinStreet = for {
+      val personBuilder = for {
         address <- builder[Address]
         _       <- address lines Seq("12 Maudlin Street", "Manchester")
         _       <- address postcode("VB6 5UX")
-      } yield address
-
-      val withoutAge = for {
-        address <- maudlinStreet
 
         person  <- builder[Person]
         _       <- person name "Stephen Patrick"
         _       <- person address address
       } yield person
 
-      buildFrom(withoutAge) must beEqualTo(Person("Stephen Patrick", None, buildFrom(maudlinStreet)))
+      buildFrom(personBuilder) must beEqualTo(Person(
+        "Stephen Patrick",
+        None,
+        Address(Seq("12 Maudlin Street", "Manchester"), "VB6 5UX")))
+    }
+
+    """Be able to compose recordings within the recorder monad""" in {
+      val addressBuilder = for {
+        address <- builder[Address]
+        _       <- address lines Seq("12 Maudlin Street", "Manchester")
+        _       <- address postcode("VB6 5UX")
+      } yield address
+
+      val personBuilder = for {
+      address <- addressBuilder
+      person  <- builder[Person]
+      _       <- person name "Stephen Patrick"
+      _       <- person address address
+      } yield person
+
+      buildFrom(personBuilder) must beEqualTo(Person(
+        "Stephen Patrick",
+        None,
+        Address(Seq("12 Maudlin Street", "Manchester"), "VB6 5UX")))
+    }
+
+    """Be able to extend an existing recording, filling in missing values""" in {
+      val withoutAge = for {
+        address <- builder[Address]
+        _       <- address lines Seq("12 Maudlin Street", "Manchester")
+        _       <- address postcode("VB6 5UX")
+
+        person  <- builder[Person]
+        _       <- person name "Stephen Patrick"
+        _       <- person address address
+      } yield person
 
       val withAge = for {
         person <- withoutAge
         _      <- person age 63
       } yield person
 
-      buildFrom(withAge) must beEqualTo(Person("Stephen Patrick", Some(63), buildFrom(maudlinStreet)))
+      buildFrom(withAge) must beEqualTo(Person(
+        "Stephen Patrick",
+        Some(63),
+        Address(Seq("12 Maudlin Street", "Manchester"), "VB6 5UX")))
+    }
+
+    """Be able to override details of an existing recording""" in {
+      val personBuilder = for {
+        address <- builder[Address]
+        _       <- address lines Seq("12 Maudlin Street", "Manchester")
+        _       <- address postcode("VB6 5UX")
+
+        person  <- builder[Person]
+        _       <- person name "Stephen Patrick"
+        _       <- person address address
+      } yield person
 
       val movedToDulwich = for {
         newAddress <- builder[Address]
         _          <- newAddress lines Seq("42 Penguin Ave", "Dulwich")
         _          <- newAddress postcode("E17 4TW")
 
-        person <- withoutAge
+        person <- personBuilder
         _      <- person address newAddress
       } yield person
 
-      buildFrom(movedToDulwich) must beEqualTo(Person("Stephen Patrick", None, Address(Seq("42 Penguin Ave", "Dulwich"), "E17 4TW")))
+      buildFrom(movedToDulwich) must beEqualTo(Person(
+        "Stephen Patrick",
+        None,
+        Address(Seq("42 Penguin Ave", "Dulwich"), "E17 4TW")))
+    }
+
+    """Convert iterables of builders into reified objects when building""" in {
+      val personBuilder = for {
+        address <- builder[Address]
+        _       <- address lines Seq("12 Maudlin Street", "Manchester")
+        _       <- address postcode("VB6 5UX")
+
+        person  <- builder[Person]
+        _       <- person name "Stephen Patrick"
+        _       <- person address address
+      } yield person
 
       val withHobbies = for {
         hobby1 <- builder[Hobby]
@@ -74,14 +135,62 @@ class BuildersSpec extends Specification {
         _      <- hobby2 name "Ice skating"
         _      <- hobby2 hoursPerWeek 2
 
-        person <- withoutAge
+        person <- personBuilder
         _      <- person hobbies Set(hobby1, hobby2)
       } yield person
 
-      buildFrom(withHobbies) must beEqualTo(Person("Stephen Patrick",
-                                                   None,
-                                                   buildFrom(maudlinStreet),
-                                                   Set(Hobby("Programming", 5), Hobby("Ice skating", 2))))
+      buildFrom(withHobbies) must beEqualTo(Person(
+        "Stephen Patrick",
+        None,
+        Address(Seq("12 Maudlin Street", "Manchester"), "VB6 5UX"),
+        Set(Hobby("Programming", 5), Hobby("Ice skating", 2))))
+    }
+
+    """Convert a 2Tuple of builders into a 2Tuple of objects""" in {
+      val personAndAddressBuilder = for {
+        address <- builder[Address]
+        _       <- address lines Seq("12 Maudlin Street", "Manchester")
+        _       <- address postcode("VB6 5UX")
+
+        person  <- builder[Person]
+        _       <- person name "Stephen Patrick"
+        _       <- person address address
+      } yield (person, address)
+
+      val (person, address) = buildFrom(personAndAddressBuilder)
+
+      address must beEqualTo(Address(Seq("12 Maudlin Street", "Manchester"), "VB6 5UX"))
+      person must beEqualTo(Person(
+        "Stephen Patrick",
+        None,
+        address))
+    }
+
+    """Convert a 3Tuple of builders into a 3Tuple of objects""" in {
+      val tupleBuilder = for {
+        address <- builder[Address]
+        _       <- address lines Seq("12 Maudlin Street", "Manchester")
+        _       <- address postcode("VB6 5UX")
+
+        hobby1 <- builder[Hobby]
+        _      <- hobby1 name "Programming"
+        _      <- hobby1 hoursPerWeek 5
+
+        person  <- builder[Person]
+        _       <- person name "Stephen Patrick"
+        _       <- person address address
+        _       <- person hobbies Set(hobby1)
+      } yield (person, address, hobby1)
+
+      val (person, address, hobby) = buildFrom(tupleBuilder)
+
+      address must beEqualTo(Address(Seq("12 Maudlin Street", "Manchester"), "VB6 5UX"))
+      hobby must beEqualTo(Hobby("Programming", 5))
+      person must beEqualTo(Person(
+        "Stephen Patrick",
+        None,
+        address,
+      Set(hobby)))
     }
   }
 }
